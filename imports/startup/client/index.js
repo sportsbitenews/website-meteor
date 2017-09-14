fe2b2cd36211ebd021bc3e68292cbac6be9fd3de
mattpen <matthew.pennington@colorado.edu>
2017-09-14 11:44:55 -0600

    Removed overpass, added window url state replace to remove extraneous route

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,3 +2,6 @@
 // Import client startup through a single index entry point
 
 import './routes.js';
+import {MOUNT_PATH} from '/imports/api/data/constants.js';
+
+window.onload = () => { window.history.replaceState( null, null, window.location.href.replace( MOUNT_PATH, '' ) ); };
