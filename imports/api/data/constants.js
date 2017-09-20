981668fbf489d42897730ef0551f3467184653f8
mattpen <matthew.pennington@colorado.edu>
2017-09-20 10:38:12 -0600

    Updated call for secret server code

--- imports/api/data/constants.js
+++ imports/api/data/constants.js
@@ -5,14 +5,22 @@
  * @author Matt Pennington
  **/
 
-let origin = window.location.origin;
+import {Meteor} from 'meteor/meteor';
 
-// If origin contains a port number, strip it out.  For local deployment.
-const secondColon = origin.indexOf( ':', origin.indexOf( ':' ) + 1 );
-if ( secondColon !== -1 ) {
-  origin = origin.substr( 0, secondColon )
+let origin;
+if ( Meteor.isServer ) {
+  origin = 'http://127.0.0.1'
+}
+else {
+  origin = window.location.origin;
+
+  // If origin contains a port number, strip it out.  For local deployment.
+  const secondColon = origin.indexOf( ':', origin.indexOf( ':' ) + 1 );
+  if ( secondColon !== -1 ) {
+    origin = origin.substr( 0, secondColon )
+  }
 }
 
-export const PUBLIC_ORIGIN = origin;
+export const PUBLIC_ORIGIN = Meteor.isServer ? 'http://127.0.0.1' : origin;
 export const MOUNT_PATH = '/_m';
 export const ENGLISH_LOCALE = 'en';
